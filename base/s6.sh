65de6504113405a4065661d05fe784f9b14daf38
helto4real <tomas.hellstrom@yahoo.se>
2023-10-21 22:09:21 +0200

    upgrade s6

--- base/s6.sh
+++ base/s6.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# https://github.com/just-containers/s6-overlay
-S6_VERSION="v2.2.0.3"
+S6_VERSION="3.1.5.0"
+
+wget -nv -O  /tmp/s6-overlay-noarch.tar.xz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-noarch.tar.xz"
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz 
+
 ARCH=$(uname -m)
 
 if [ $ARCH == "armv7l" ]; then
-    wget -q -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-armhf.tar.gz"
+    wget -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-armhf.tar.xz"
 elif [ $ARCH == "aarch64" ]; then
-    wget -q -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-aarch64.tar.gz"
+    wget -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-aarch64.tar.xz"
 elif [ $ARCH == "x86_64" ]; then
-    wget -q -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-amd64.tar.gz"
+    wget -nv -O /tmp/s6.tar.xz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-x86_64.tar.xz"
 else
     echo 'NOT SUPPORTED ARCH: $ARCH'; exit 1; 
 fi
-
-tar xzf /tmp/s6.tar.gz -C /
-rm /tmp/s6.tar.gz
+tar -C / -Jxpf /tmp/s6.tar.xz
+rm /tmp/s6.tar.xz
+rm /tmp/s6-overlay-noarch.tar.xz
