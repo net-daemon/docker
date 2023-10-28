54620100d26882fd999af60d1f8c82f08fc6c3dd
helto4real <tomas.hellstrom@yahoo.se>
2023-10-28 13:27:26 +0200

    bug fix on arm7 builds

--- base/s6.sh
+++ base/s6.sh
@@ -6,7 +6,7 @@ tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
 
 ARCH=$(uname -m)
 
-if [ $ARCH == "armv7l" ]; then
+if [ $ARCH == "armv7" ]; then
     wget -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-armhf.tar.xz"
 elif [ $ARCH == "aarch64" ]; then
     wget -nv -O /tmp/s6.tar.gz "https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-aarch64.tar.xz"
